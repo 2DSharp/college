bd392f4bfa486f82a1ba29b8849a13f0d2e84e05
Dedipyaman Das <30053071+2Dsharp@users.noreply.github.com>
2017-12-29 12:52:36 +0530

    PageRankPrimitive

---
+++ Winter-17/MATLAB/PageRank.m
@@ -0,0 +1,23 @@
+% Full probability matrix 6x6
+pM = [0 1/3 0 0 1/3 1/3; 0 0 1/3 0 1/3 0; 1 0 0 0.5 0 1/3; 0 1/3 0 0 0 1/3; 0 1/3 1/3 0 0 0; 0 0 1/3 0.5 1/3 0];
+
+% Probabilty matrix for 0 clicks
+pMn = [0; 1; 0; 0; 0; 0];
+
+n = input('Enter the number of clicks: ');
+
+for c = 1:n
+% Probabilty matrix for req click
+    pMn = pM * pMn;
+end
+max = 0;
+    for j = 1:6
+        if pMn(j, 1) > max
+            max = pMn(j, 1);
+            maxVisited = j;
+        end
+    end
+%pMn
+disp("Highest visited page: ")
+maxIndex
+max
