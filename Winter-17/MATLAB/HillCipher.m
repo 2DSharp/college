cb282e12eb8db151093ce068f3bd5cd8d844a295
Dedipyaman Das <2DSharp@github.com>
2018-01-12 14:14:23 +0530

    Hill Encryption and decryption works!

--- Winter-17/MATLAB/HillCipher.m
+++ Winter-17/MATLAB/HillCipher.m
@@ -1,3 +1,5 @@
+% Encryption
+
 plaintext = input('Enter the text', 's');
 matText = double(plaintext);
 key = [ 1 0; 0 3];
@@ -8,8 +10,9 @@ if mod(b, 2) ~ 0
 end
 
 for i = [1:2:size(matText, 2)]
-  pair = [mod(matText(i), 64) mod(matText(i+1), 64)];
 
+  pair = [mod(matText(i), 64) mod(matText(i+1), 64)];
+  
   cipherBlock = pair * key;
   cipherMat(end + 1) = cipherBlock(1);
   cipherMat(end + 1 ) = cipherBlock(2);
@@ -17,7 +20,16 @@ for i = [1:2:size(matText, 2)]
 end
 cipherText = char(cipherMat)
 
-%if ( == 0)
-  %matText(1, size(matText, 2))
-	
-		       
+
+				% Decryption
+plainMat = [ ];
+matText = double(cipherText);
+for i = [1:2:size(matText, 2)]
+  
+  pair = [matText(i); matText(i+1)];
+  plainBlock = inv(key) * pair;
+  plainMat(end + 1) = plainBlock(1)+ 64;
+  plainMat(end + 1) = plainBlock(2) + 64;
+end
+
+plaintext = char(plainMat)
